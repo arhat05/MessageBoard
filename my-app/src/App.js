d38f8aef8758cc118f66306a3a44c51a11738012
Arhat Shah <arhat.p.shah@gmail.com>
2023-12-07 20:50:21 -0500

    sorta working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import MessageForm from './components/MessageForm';
 import MessageList from './components/Messages';
 import './styles/App.css';
@@ -6,19 +6,30 @@ import './styles/App.css';
 const App = () => {
   const [messages, setMessages] = useState([]);
 
+
+
   const handleAddMessage = (message) => {
-    setMessages([message, ...messages]);
+  setMessages([message, ...messages]);
+  
+    //window.location.reload();
+    // refresh messages component so that it will fetch the new message from the server
+
+    
+
   };
 
-  
+
   return (
     <div>
       <div className="message-form">
         <h1>Message Board</h1>
-        <MessageForm onMessageSubmit={handleAddMessage} />
+        <MessageForm 
+        onMessageSubmit={(newMessage) => {
+          handleAddMessage(newMessage);
+        }} />
       </div>
       <div className="container">
-        <MessageList messages={messages} />
+        <MessageList messages={setMessages} />
       </div>
     </div>
   );
--- my-app/src/components/MessageForm.js
+++ my-app/src/components/MessageForm.js
@@ -1,42 +1,38 @@
-import React, { useState } from 'react';
-import { useNavigate } from "react-router";
+import React, { useState} from 'react';
+import { useNavigate } from 'react-router';
 
-const MessageForm = ({ onMessageSubmit }) => {
+const MessageForm = ({ onMessageSubmit, updateMessages }) => {
   const [message, setMessage] = useState('');
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   if (message.trim() !== '') {
-  //     const timestamp = new Date(); // Add timestamp
-  //     onMessageSubmit({ text: message, timestamp });
-  //     setMessage('');
-  //   }
-  // };
-
   const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
     if (message.trim() !== '') {
-      const timestamp = new Date(); // Add timestamp
+      const timestamp = new Date();
       onMessageSubmit({ text: message, timestamp });
       setMessage('');
+
+      const data = { text: message, timestamp };
+      const response = await fetch('http://localhost:5000/msgs/add', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        updateMessages(); // Fetch and update messages after posting
+      } else {
+        console.error('Failed to post message:', response.statusText);
+      }
+
+      navigate('/');
     }
-    const timestamp = new Date();
-    const data = { text: message, timestamp };
-    const response = await fetch("http://localhost:5000/msgs/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    const json = await response.json();
-    console.log(json);
-    navigate("/");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <textarea className='text-area'
+      <textarea
+        className="text-area"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         maxLength={128}
